5dc36dccfba3ff4dd8eb0cbd72a6c9db85ec6bab
unknown <analitico01@gmail.com>
2021-03-29 22:34:32 -0300

    erro de requisicao assincrona

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::io;
+use futures::executor::block_on;
+
+fn get_user_data(data: String) {
+    // println!("{}", data);
+    let mut body = reqwest::get("https://www.rust-lang.org");
+    println!("body = {:?}", body);
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("********************");
+    println!("**************************");
+    println!("********************************");
+    println!("**************************************");
+
+    println!("Por favor, digite seu nome de usuário.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+        let internet_data = get_user_data(guess);
+
+
+        println!("Ok... Verificando...");
+
+
+        block_on(internet_data);
+    
+        println!("**************************************");
+        println!("********************************");
+        println!("**************************");
+        println!("********************");
+    // println!("You guessed: {}", guess);
 }
